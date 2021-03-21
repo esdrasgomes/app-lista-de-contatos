6bc86bede01f7cffe3369ea660d7c499202bc833
Esdras Gomes <esdrasgoomes@gmail.com>
2021-03-21 19:47:21 -0300

    Criando o menu de opções

--- app/src/main/java/com/example/appdecontatos/MainActivity.kt
+++ app/src/main/java/com/example/appdecontatos/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.appdecontatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -41,4 +46,28 @@ class MainActivity : AppCompatActivity() {
         )
     }
 
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    // Pegando o item que foi clicado a partir do id
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.item_menu_1 -> {
+                showToast("Exibindo item de menu 1")
+                return true
+            }
+            R.id.item_menu_2 -> {
+                showToast("Exibindo item de menu 2")
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
